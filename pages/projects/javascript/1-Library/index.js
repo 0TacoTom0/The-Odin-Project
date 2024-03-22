d06ecee4981caf05b1defc494bf433caac327f33
0TacoTom0 <tombowcutt@outlook.com>
2024-03-22 16:22:40 +0000

    Redo Library

--- pages/projects/javascript/1-Library/index.js
+++ pages/projects/javascript/1-Library/index.js
@@ -1,118 +1,99 @@
-const addBookBtn = document.getElementById('addBookBtn');
-const overlay = document.getElementById("bookOverlay");
+const dialog = document.querySelector("dialog");
+const showButton = document.getElementById('addBookBtn');
+const closeButton = document.querySelector("dialog button");
+const submitBtn = document.getElementById('submitBtn');
+const bookContainer = document.getElementById('bookContainer');
+const userBookTitleInput = document.getElementById('userBookTitle');
+const userBookStatus = document.getElementById('userStatus');
+const form = document.getElementById('myForm');
 
-let objectArr = [];
-let item1 = new Book('Tom', 'true')
-let item2 = new Book('Bruh', 'false')
-objectArr.push(item1)
-objectArr.push(item2)
-
-addBookBtn.addEventListener('click', showForm);
-
-function showForm() {
-    overlay.style.display = "flex";
-}
-
-function hideForm() {
-    overlay.style.display = "none";
-    document.getElementById("form").reset();
-}
-
-let bool = true;
-function getClickedElem(id) {
-    if (bool) {
-        bool = false
-        if (id == 'bookOverlay') hideForm();
-        setTimeout(function() {
-            bool = true; 
-        }, 100);
+const myLibrary = [
+    {
+        name: 'Book One',
+        isRead: true
+    },
+    {
+        name: 'Book Two',
+        isRead: false
     }
-}
+];
 
-function submitForm() {
-    event.preventDefault();
-    getResults();
-    hideForm();
-}
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+});
 
-function changeState(elem, button) {
-    if (elem.status == 'true') {
-        elem.status = 'false';
-        button.classList.replace('readState', 'unreadState');
-        button.innerHTML = 'Unread';
-    }
-    else{
-        elem.status = 'true';
-        button.classList.replace('unreadState', 'readState');
-        button.innerHTML = 'Read';
-    }
-}
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
 
-function Book(name, status) {
-    this.name = name;
-    this.status = status;
-}
+function Book() {
+    while (bookContainer.firstChild) {
+        bookContainer.removeChild(bookContainer.lastChild);
+    };
 
-function removeBook(elem) {
-    objectArr.splice(objectArr.indexOf(elem), 1);
-    renderBooks();
-}
+    myLibrary.forEach(element => {
+        const bookDiv = document.createElement("div");
+        const nameDiv = document.createElement("div");
+        const btnDiv = document.createElement("div");
+        const bookTitle = document.createElement("h3");
+        const bookNumber = document.createElement("p");
+        const stateBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
 
-function getResults() {
-    let tempName = document.getElementById('titleInput').value;
-    let hasReadBook = document.querySelector('input[name="readStatus"]:checked').value;
-    let newBook = new Book(tempName, hasReadBook);
-    objectArr.push(newBook);
-    renderBooks();
-}
+        bookTitle.innerHTML = element.name;
+        bookNumber.innerHTML = `#${myLibrary.indexOf(element) + 1}`;
+        stateBtn.innerHTML = `${element.isRead ? 'Read' : 'Unread'}`;
+        deleteBtn.innerHTML = 'Delete';
 
-function renderBooks() {
-    const currentDiv = document.getElementById("bookContainer");
-    while (currentDiv.firstChild) {
-        currentDiv.removeChild(currentDiv.firstChild);
-    }
-    objectArr.forEach(elem => {
-        const newDiv = document.createElement('div');
-        const textDiv = document.createElement('div');
-        const title = document.createElement('h2');
-        const bookNum = document.createElement('p');
+        bookDiv.classList.add("book");
+        stateBtn.setAttribute("id", `${element.isRead ? 'readStateBtn' : 'unreadStateBtn'}`);
+        deleteBtn.setAttribute("id", myLibrary.indexOf(element));
+        deleteBtn.classList.add('deleteButton');
 
-        newDiv.classList.add('bookDiv');
-        title.innerHTML = elem.name;
-        bookNum.innerHTML = `#${objectArr.indexOf(elem)+1}`;
-
-        textDiv.appendChild(title);
-        textDiv.appendChild(bookNum);
-        newDiv.appendChild(textDiv);
-
-        const buttonDiv = document.createElement('div');
-        const buttonRead = document.createElement('button');
-        const buttonDelete = document.createElement('button');
-
-        buttonDiv.classList.add('bookButtonContainer');
-        if (elem.status == 'true') {
-            buttonRead.innerHTML = 'Read';
-            buttonRead.classList.add('readState');
-        }
-        else {
-            buttonRead.innerHTML = 'Unread'
-            buttonRead.classList.add('unreadState');
-        }
-        buttonRead.addEventListener('click', function() {
-            changeState(elem, buttonRead);
+        stateBtn.addEventListener("click", () => {
+            if (stateBtn.innerHTML == 'Read') {
+                stateBtn.innerHTML = 'Unread';
+                stateBtn.setAttribute("id", "unreadStateBtn");
+            }
+            else{
+                stateBtn.innerHTML = 'Read';
+                stateBtn.setAttribute("id", "readStateBtn");
+            }
         });
-        buttonDelete.innerHTML = 'Delete';
-        buttonDelete.classList.add('deleteButton');
-        buttonDelete.addEventListener('click', function() {
-            removeBook(elem);
+        deleteBtn.addEventListener("click", () => {
+            removeBookFromLibrary(deleteBtn.id);
         });
 
-        buttonDiv.appendChild(buttonRead);
-        buttonDiv.appendChild(buttonDelete);
-        newDiv.appendChild(buttonDiv);
-
-        currentDiv.appendChild(newDiv);
+        bookDiv.appendChild(nameDiv);
+        bookDiv.appendChild(btnDiv);
+        nameDiv.appendChild(bookTitle);
+        nameDiv.appendChild(bookNumber);
+        btnDiv.appendChild(stateBtn);
+        btnDiv.appendChild(deleteBtn);
+        bookContainer.appendChild(bookDiv);
     });
 }
 
-renderBooks();
+function addBookToLibrary() {
+    let bookToAdd = {
+        name: `${userBookTitleInput.value}`,
+        isRead: userBookStatus.value == 'true' ? true : false
+    };
+    myLibrary.push(bookToAdd);
+
+    userBookTitleInput.value = ''
+    userBookStatus.value = 'true'
+
+    Book();
+    dialog.close();
+    event.preventDefault();
+}
+
+function removeBookFromLibrary(index) {
+    if (index > -1) {
+        myLibrary.splice(index, 1);
+    };
+    Book();
+}
+
+Book();
